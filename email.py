0a29668c9cbcf04a39ca668d0e2615972423d265
Brittney Pugh <brittneypugh@brittneys-mbp.mynetworksettings.com>
2024-05-10 21:13:13 -0400

    Added Functions and Generated Email List

---
+++ email.py
@@ -0,0 +1,43 @@
+def userInput():
+
+    first_name = input("Enter the recruiter's first name: ")
+    last_name = input("Enter the recruiter's last name: ")
+    email_domain = input("Enter the email domain: ")
+    return first_name, last_name, email_domain
+
+
+
+def createEmail(first, last, domain, email_list):
+
+    format1 = str(first) + str(last) + "@" + str(domain)
+    print(format1)
+    format2 = str(first) + "." + str(last) + "@" + str(domain)
+    print(format2)
+    format3 = str(first) + "_" + str(last) + "@" + str(domain)
+    print(format3)
+    format4 = str(first) + "-" + str(last) + "@" + str(domain)
+    print(format4)
+
+    email_list.extend([format1,format2,format3,format4])
+    return email_list
+    
+
+
+
+
+def main_function():
+    info = userInput()
+    email_list = []
+
+    first = info[0]
+    first_int = first[0]
+    last = info[1]
+    last_int = last[0]
+    domain = info[2]
+    
+    email_list = createEmail(first,last,domain, email_list)
+    email_list = createEmail(first_int,last,domain, email_list)
+    email_list = createEmail(first,last_int,domain, email_list)
+    print(email_list)
+
+main_function()
